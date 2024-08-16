8b8c95f6af1507d8ea9ff5e1c256a2d39463e52f
anastasia-zh97 <abaabady-97@yandex.ru>
2024-08-16 17:35:46 +0300

    changed 11 case again

--- File2/src/main/kotlin/Main.kt
+++ File2/src/main/kotlin/Main.kt
@@ -1,10 +1,9 @@
 package ru.Netology
 
 fun main() {
-    var likes = 11
-    var result = likes % 100
+    var likes = 211
     var text = " "
-    if (likes % 10 == 1 && result != 11) {
+    if (likes % 10 == 1 && likes % 100 != 11) {
         text = "человеку"
     } else {
         text = "людям"
